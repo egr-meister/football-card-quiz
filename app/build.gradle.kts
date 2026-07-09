f42202c0d6134a59fb748ee7dbd7057e282f6935
egr-meister <dulingleb@gmail.com>
2026-07-09 03:55:46 +0300

    Fix Retrofit kotlinx-serialization converter dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -147,7 +147,7 @@ dependencies {
 
     // Networking (secondary Match Schedule feature only)
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation("com.squareup.retrofit2:converter-kotlinx-serialization:2.11.0")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
